e03eed97a8e4af9b28e2d88c4e2a79e11876b7ff
Mohammad Shahab <shahabkovict@gmail.com>
2022-04-05 14:59:23 +0530

    changes done

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,24 @@
 const http = require('http')
-const app = require('./app');
+const app = require('./app')
+const mongoose = require('mongoose')
 const {loadPlanetsData} = require('./models/plantes.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://shahab:testing123456@nasacluster.ojtgt.mongodb.net/isro?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open',()=>{
+    console.log("MongoDb connection ready");
+});
+
+mongoose.connection.on('error',(err)=>{
+    console.error(err); 
+})
+
 async function startServer(){
+    await mongoose.connect(MONGO_URL)
     await loadPlanetsData();
 
     server.listen(PORT,()=>{
